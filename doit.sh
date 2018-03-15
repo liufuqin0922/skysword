31955631799cf18f50dd5307317cc412d09fed90
skysword <liufuqin0922@hotmail.com>
2018-03-15 23:01:14 +0800

    新文件：   doit.sh

---
+++ doit.sh
@@ -0,0 +1,21 @@
+# iconv -f GBK -t UTF-8 A* >B.txt
+# rm A*
+# awk -F',' '$3~/^1/ {print $3}' B.txt | awk '!a[$0]++' >isdn.txt
+# grep -f shbj.txt isdn.txt >xxhh.txt
+# grep -f xxhh.txt B.txt >>result.txt
+
+awk -F, '$2>0 && $8>0 {print $2}' B.txt | sort | uniq -c | sort -k1nr | awk '$1>100' >test
+while read num line
+do
+	isdn=${line:0:7}
+	aa=`grep $isdn isdn_all.txt`
+	awk -F, '$2=="'$line'" && $8>0 {print $3,$6,$8/60000}' B.txt | while read other calltype dur
+	do
+		others=${other:0:7}
+		bb=`grep $others isdn_all.txt`
+		echo $num $line ${aa:8} $other ${bb:8} $calltype $dur
+	done 
+done < test
+
+# do echo;echo $i;export a=`expr substr $i 1 7`;grep $a isdn_all.txt;
+# done >test.txt
