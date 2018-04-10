5227ee42bb574307a91e88ccbda450e231367817
liufuqin0922 <liufuqin0922@hotmail.com>
2018-04-10 18:16:05 +0800

    新文件：   sh600012-2018-01-10.xls

--- get_isdn_info_from_360.py
+++ get_isdn_info_from_360.py
@@ -1,5 +1,6 @@
 import requests
 from lxml import etree
+import time
 
 import sys
 
@@ -10,12 +11,30 @@ else:
 
 
 def getISDN(isdn):
+    if len(isdn) < 11:
+        tmp = "0760"+isdn
+	isdn = tmp
     url = "https://www.so.com/s?q=" + isdn
 
     s = requests.Session()
     html_context = s.get(url).content
 
     isdn_info = etree.HTML(html_context)
+    isdn_home = isdn_info.xpath('//p[@class="mh-detail"]/text()')
+    if len(isdn_home) >= 1:
+        aa = isdn_home[0].encode('utf-8')
+        a = aa[11:22]
+        b = aa[59:65]
+        c = aa[77:83]
+        d = aa[119:125]
+        print a, b, c, d
+
+    isdn_homev2 = isdn_info.xpath('//div[@class="gclearfix mh-detail"]/span/text()')
+    if len(isdn_homev2) >= 1:
+        aa = isdn_homev2[0].encode('utf-8')
+        bb = isdn_homev2[1].encode('utf-8')
+        print aa[10:21], bb[20:26], bb[37:43], bb[76:82]
+
 
     table = isdn_info.xpath("//span[@style]/text()")
     if len(table) > 4:
@@ -28,3 +47,4 @@ def getISDN(isdn):
 
 for line in open(sys.argv[1]):
     getISDN(line)
+#    time.sleep(1)
